46ff7b7fd7bb9882ee249206755cf83767aebfcc
Rocky Warren <rocky@dwolla.com>
2013-08-07 12:04:09 -0500

    Add synchronous service, rev version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# `dwolla-java-sdk v1.0.1`
+# `dwolla-java-sdk v1.1.0`
 
 [![Build Status](https://travis-ci.org/therockstorm/dwolla-java-sdk.png?branch=master)](https://travis-ci.org/therockstorm/dwolla-java-sdk)
 
@@ -6,7 +6,7 @@ The [Dwolla API](http://developers.dwolla.com/dev) wrapper for Java and Android.
 
 ## Quick Start
 
-This wrapper uses the [Retrofit](https://github.com/square/retrofit) REST client. See the [DwollaService](https://github.com/therockstorm/dwolla-java-sdk/blob/master/src/main/java/com/dwolla/java/sdk/DwollaService.java) class for the supported endpoints and Retrofit's [sample](https://github.com/square/retrofit/blob/master/samples/twitter-client/src/main/java/com/squareup/retrofit/sample/twitter/Client.java) for more details.
+This wrapper uses the [Retrofit](https://github.com/square/retrofit) REST client. See the DwollaServiceAsync or DwollaServiceSync classes for the supported endpoints and Retrofit's [sample](https://github.com/square/retrofit/blob/master/samples/twitter-client/src/main/java/com/squareup/retrofit/sample/twitter/Client.java) for more details.
 
 Create a callback that extends DwollaCallback with the correct Response object:
 ```java
@@ -27,12 +27,12 @@ private class BasicInformationCallback extends DwollaCallback<BasicAccountInform
 ```
 Then create the service and make the API call:
 ```java
-DwollaService service = new RestAdapter.Builder().setServer(
-   new Server("https://www.dwolla.com/oauth/rest")).build().create(DwollaService.class);
+DwollaServiceAsync service = new RestAdapter.Builder().setServer(
+   new Server("https://www.dwolla.com/oauth/rest")).build().create(DwollaServiceAsync.class);
 service.getBasicAccountInformation(accountId, mEncodedKey, mEncodedSecret,
    new BasicInformationCallback());
 ```
-Post requests are just as simple. After creating a callback and DwollaService as shown above, make the API call:
+Post requests are just as simple. After creating a callback and DwollaServiceAsync as shown above, make the API call:
 ```java
 service.send(new DwollaTypedBytes(new Gson(),
    new SendRequest(mToken, pin, destinationId, amount)), new SendCallback());
---
+++ src/main/java/com/dwolla/java/sdk/DwollaServiceAsync.java
@@ -0,0 +1,109 @@
+package com.dwolla.java.sdk;
+
+import com.dwolla.java.sdk.responses.AccountInformationResponse;
+import com.dwolla.java.sdk.responses.BalanceResponse;
+import com.dwolla.java.sdk.responses.BasicAccountInformationResponse;
+import com.dwolla.java.sdk.responses.DepositWithdrawResponse;
+import com.dwolla.java.sdk.responses.FulfillRequestResponse;
+import com.dwolla.java.sdk.responses.FundingSourcesByIdResponse;
+import com.dwolla.java.sdk.responses.FundingSourcesListingResponse;
+import com.dwolla.java.sdk.responses.NearbySpotsResponse;
+import com.dwolla.java.sdk.responses.NearbyUsersResponse;
+import com.dwolla.java.sdk.responses.PendingRequestsResponse;
+import com.dwolla.java.sdk.responses.RequestResponse;
+import com.dwolla.java.sdk.responses.SendResponse;
+import com.dwolla.java.sdk.responses.TransactionsResponse;
+import com.dwolla.java.sdk.responses.UserContactsResponse;
+
+import retrofit.Callback;
+import retrofit.http.Body;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
+/**
+ * Asynchronous Dwolla API endpoints, see <a href="https://developers.dwolla.com/dev/docs/">developers.dwolla.com/dev/docs</a> for more information.
+ */
+@SuppressWarnings("UnusedDeclaration")
+public interface DwollaServiceAsync {
+
+   /**
+    * Balance
+    */
+   @GET("/balance/")
+   void getBalance(@Query(Consts.Api.TOKEN) String oauthToken, Callback<BalanceResponse> callback);
+
+   /**
+    * Contacts
+    */
+   @GET("/contacts/nearby")
+   void getNearbySpots(@Query(Consts.Api.CLIENT_ID) String clientId, @Query(Consts.Api.CLIENT_SECRET) String clientSecret,
+                       @Query(Consts.Api.LATITUDE) double latitude, @Query(Consts.Api.LONGITUDE) double longitude,
+                       @Query(Consts.Api.RANGE) int range, @Query(Consts.Api.LIMIT) int limit, Callback<NearbySpotsResponse> callback);
+
+   @GET("/contacts/")
+      // For types, see Consts.UserContactType
+   void getUserContacts(@Query(Consts.Api.TOKEN) String oauthToken, @Query(Consts.Api.SEARCH) String search,
+                        @Query(Consts.Api.TYPES) String types, @Query(Consts.Api.LIMIT) int limit, Callback<UserContactsResponse> callback);
+
+   /**
+    * Funding Sources
+    */
+   @GET("/fundingsources/{funding_id}")
+   void getFundingSourcesById(@Query(Consts.Api.TOKEN) String oauthToken, @Path(Consts.Api.FUNDING_ID) String funding_id,
+                              Callback<FundingSourcesByIdResponse> callback);
+
+   @GET("/fundingsources/")
+   void getFundingSourcesListing(@Query(Consts.Api.TOKEN) String oauthToken, Callback<FundingSourcesListingResponse> callback);
+
+   @POST("/fundingsources/{funding_id}/deposit")
+   void deposit(@Body DwollaTypedBytes request, @Path(Consts.Api.FUNDING_ID) String funding_id,
+                Callback<DepositWithdrawResponse> callback);
+
+   @POST("/fundingsources/{funding_id}/withdraw")
+   void withdraw(@Body DwollaTypedBytes request, @Path(Consts.Api.FUNDING_ID) String funding_id,
+                 Callback<DepositWithdrawResponse> callback);
+
+   /**
+    * Requests
+    */
+   @GET("/requests/")
+   void getPendingRequests(@Query(Consts.Api.TOKEN) String oauthToken, Callback<PendingRequestsResponse> callback);
+
+   @POST("/requests/{request_id}/fulfill")
+   void fulfillRequest(@Body DwollaTypedBytes request, @Path(Consts.Api.REQUEST_ID) String request_id,
+                       Callback<FulfillRequestResponse> callback);
+
+   @POST("/requests/")
+   void request(@Body DwollaTypedBytes request, Callback<RequestResponse> callback);
+
+   /**
+    * Transactions
+    */
+   @GET("/transactions/")
+   // For types, see Consts.TransactionType
+   void getTransactions(@Query(Consts.Api.TOKEN) String oauthToken, @Query(Consts.Api.CLIENT_ID) String clientId,
+                        @Query(Consts.Api.CLIENT_SECRET) String clientSecret, @Query(Consts.Api.SINCE_DATE) String sinceDate,
+                        @Query(Consts.Api.TYPES) String types, @Query(Consts.Api.LIMIT) int limit,
+                        @Query(Consts.Api.SKIP) int skip, @Query(Consts.Api.GROUP_ID) String groupId, Callback<TransactionsResponse> callback);
+
+   @POST("/transactions/send")
+   void send(@Body DwollaTypedBytes request, Callback<SendResponse> callback);
+
+   /**
+    * Users
+    */
+   @GET("/users/")
+   void getAccountInformation(@Query(Consts.Api.TOKEN) String oauthToken, Callback<AccountInformationResponse> callback);
+
+   @GET("/users/{account_identifier}")
+   void getBasicAccountInformation(@Path(Consts.Api.ACCOUNT_ID) String accountIdentifier, @Query(Consts.Api.CLIENT_ID) String clientId,
+                                   @Query(Consts.Api.CLIENT_SECRET) String clientSecret, Callback<BasicAccountInformationResponse> callback);
+
+   @GET("/users/nearby")
+   void getNearbyUsers(@Query(Consts.Api.CLIENT_ID) String clientId, @Query(Consts.Api.CLIENT_SECRET) String clientSecret,
+                       @Query(Consts.Api.LATITUDE) double latitude, @Query(Consts.Api.LONGITUDE) double longitude,
+                       Callback<NearbyUsersResponse> callback);
+
+}
